491296988232226deb4a31c5b9c7ecbfcb210060
Alex Xavier <alexxavier84@gmail.com>
2016-03-12 19:50:21 +0530

    Services

--- Concept/js/finance1.js
+++ Concept/js/finance1.js
@@ -1,17 +1,31 @@
 angular.module("finance1", [])
-.factory('currencyConverter', function(){
+.factory('currencyConverter', ['$http', function($http){
+    var YAHOO_FINANCE_URL_PATTERN =
+        '//query.yahooapis.com/v1/public/yql?q=select * from '+
+        'yahoo.finance.xchange where pair in ("PAIRS")&format=json&'+
+        'env=store://datatables.org/alltableswithkeys&callback=JSON_CALLBACK';
     var currencies = ['USD', 'EUR', 'INR'];
-    var usdToForeignRates = {
-        USD: 1,
-        EUR: 0.75,
-        INR: 68
-    };
+    var usdToForeignRates = {};
     var convert = function(amout, inCurr, outCurr){
         return amout * usdToForeignRates[outCurr]/usdToForeignRates[inCurr];
     };
+    
+    var refresh = function(){
+        var url = YAHOO_FINANCE_URL_PATTERN.replace('PAIRS', 'USD' + currencies.join('","USD'));
+        return $http.jsonp(url).then(function(respose){
+            var newUsdToForeignRates = {};
+            angular.forEach(respose.data.query.results.rate, function(rate){
+                var currency = rate.id.substring(3,6);
+                newUsdToForeignRates[currency] = window.parseFloat(rate.Rate);
+            });
+            usdToForeignRates = newUsdToForeignRates;
+        });
+    };
+    
+    refresh();
     
     return {
         currencies: currencies,
         covert: convert
     };
-});
+}]);
