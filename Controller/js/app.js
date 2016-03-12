b4bddecb9e2e972f6282fbf5fde3b3633f55f3c3
Alex Xavier <alexxavier84@gmail.com>
2016-03-12 22:42:44 +0530

    Controller method parameter

--- Controller/js/app.js
+++ Controller/js/app.js
@@ -2,12 +2,13 @@ var myapp = angular.module('spicyApp1', []);
 
 myapp.controller('SpicyController', ['$scope', function($scope){
     $scope.spice = 'very';
+    $scope.customSpice = 'Custom Spice'
     
     $scope.chilliSpicy = function(){
       $scope.spice = 'Chilli';  
     };
     
-    $scope.jalapenoSpicy = function(){
-      $scope.spice = 'Jalapeno';  
+    $scope.customSpicy = function(customSpice){
+      $scope.spice = customSpice;  
     };
 }]);
